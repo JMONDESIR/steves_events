d5406fe4ea024b969cc344064a5a6d72b484e9df
JMONDESIR <Joey.M.Dev@gmail.com>
2018-12-06 11:01:27 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// How many total commits were made in all of Steve's events?
+for (let i = 0; i < githubData.length; i++) {
+  console.table(githubData);
+}
+
+// How many of each event type are there? (PullRequestEvent, PushEvent, etc)
+
+// List all Github users who submitted a pull request that was approved by Steve.
+
+// List all repositories on which Steve had an event, and show how many events were on each one.
+
+// Which event had the most number of commits?
+
+// Which programming langugages were affected by Steve's events?
+
+// What programming language was the most affected by Steve's events?
+
+githubData.forEach(stevent => {
+  eventTypes[stevent.type].push("something");
+});
